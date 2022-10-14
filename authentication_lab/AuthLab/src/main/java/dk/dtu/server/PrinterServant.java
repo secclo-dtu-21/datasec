469d4841fe4e05a3a1835e13b947780c0a645dc0
Agachily <zetongzhao@foxmail.com>
2022-10-14 16:32:39 +0200

    Finish topQueue

--- authentication_lab/AuthLab/src/main/java/dk/dtu/server/PrinterServant.java
+++ authentication_lab/AuthLab/src/main/java/dk/dtu/server/PrinterServant.java
@@ -2,36 +2,60 @@ package dk.dtu.server;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+
+import dk.dtu.server.entity.Printer;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 public class PrinterServant extends UnicastRemoteObject implements PrinterService {
     private static final Logger logger = LogManager.getLogger(PrinterServant.class);
+    private List<Printer> printers;
 
     protected PrinterServant() throws RemoteException {
+        printers = new ArrayList<>();
+        printers.add(new Printer("printer1"));
+        printers.add(new Printer("printer2"));
     }
 
 
     @Override
     public String print(String filename, String printer) throws RemoteException {
-        return null;
+        for (var p : printers) {
+            if (p.getName().equals(printer)) {
+                p.addFile(filename);
+                return "Success";
+            }
+        }
+        return "Can not find the specified printer";
     }
 
     @Override
     public String queue(String printer) throws RemoteException {
-        return null;
+        for (var p : printers) {
+            if (p.getName().equals(printer)) {
+                p.listQueue();
+                return "Success";
+            }
+        }
+        return "Can not find the specified printer";
     }
 
     @Override
     public String topQueue(String printer, int job) throws RemoteException {
-        return null;
+        for (var p : printers) {
+            if (p.getName().equals(printer)) {
+                p.topQueue(job);
+                return "Success";
+            }
+        }
+        return "Can not find the specified printer";
     }
 
     @Override
     public String start() throws RemoteException {
-        logger.info("Info log message");
-
-        return "test";
+        return "";
     }
 
     @Override
---
+++ authentication_lab/AuthLab/src/main/java/dk/dtu/server/entity/Printer.java
@@ -0,0 +1,55 @@
+package dk.dtu.server.entity;
+
+import dk.dtu.server.PrinterServant;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Printer {
+
+    private String name;
+    private final List<String> queue;
+    private boolean status;
+    private static final Logger logger = LogManager.getLogger(Printer.class);
+
+    public Printer(String name) {
+        this.name = name;
+        this.queue = new LinkedList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getQueue() {
+        return queue;
+    }
+
+    public void addFile(String filename) {
+        queue.add(filename);
+        logger.info(String.format("%s-%s has been added o the printing queue", this.name, filename));
+    }
+
+    public void listQueue() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%s-The current tasks are: \n", this.name));
+        for (int i = 0; i < queue.size(); i++) {
+            sb.append(String.format("<%d> <%s>\n", i + 1, queue.get(i)));
+        }
+        logger.info(sb.toString());
+    }
+
+    public void topQueue(int job) {
+        String target = queue.remove(job - 1);
+        queue.add(0, target);
+        logger.info(String.format("%s-The %dth job has been moved to the top", this.name, job));
+    }
+}
